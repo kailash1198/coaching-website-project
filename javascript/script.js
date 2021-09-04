3c61750d52bc479bb01848b1d0c8a7448bb49ea4
kailash1198 <kailash414243@gmail.com>
2021-09-05 00:00:38 +0530

    file upload

---
+++ javascript/script.js
@@ -0,0 +1,56 @@
+// alert('hello world')
+
+// document.write('my name is jhohnson.');
+
+// console.log('kailash');
+ 
+
+// variable 
+
+// var
+// let
+// const
+
+// var examples -
+// defined and also value assigned 
+
+/*var number1 = 10;
+var number2 = 100;
+var totalNumber = number1 + number2;
+console.log(totalNumber);
+
+*/
+
+/*
+var number1 =10;
+number2= 100;
+number1 = 100;
+number1= 200;
+total=number1+number2;
+console.log(total);
+
+*/
+
+// let examples 
+// not defined but value assign 
+
+/*
+let number1=100;
+let number2=200;
+number1=200;
+let number1=400;
+let totalNumber=number1+number2;
+console.log(totalNumber);
+*/
+
+// const 
+// not define and not assign value 
+
+/*
+const number1 = 100;
+const number2 = 100;
+// number1 = 200;
+// const number1 = 300;
+console.log(number1+number2);
+
+*/
